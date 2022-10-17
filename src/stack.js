694dd7004c05b9d8e58b1cab0992f14be7289f76
VaskoAndrey <nate20@mail.ru>
2022-10-17 21:37:17 +0300

    feat: complited stack task

---
+++ src/stack.js
@@ -0,0 +1,36 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+/**
+ * Implement the Stack with a given interface via array.
+ *
+ * @example
+ * const stack = new Stack();
+ *
+ * stack.push(1); // adds the element to the stack
+ * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.pop(); // returns the top element from stack and deletes it, returns 1
+ * stack.pop(); // undefined
+ *
+ */
+class Stack {
+   constructor() {
+      this.stack = [];
+      this.tail = this.stack.length
+   }
+  push( element) {
+      this.stack.push(element)
+  }
+
+  pop() {
+      return this.stack.pop()
+  }
+
+  peek() {
+      let k = this.stack;
+      return this.stack[k.length-1]
+  }
+}
+
+module.exports = {
+  Stack
+};
